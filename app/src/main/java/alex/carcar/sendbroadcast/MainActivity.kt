8027e429cea65900c452fd3920bca66cedebf35b
Alex Carcar <44008979+acarcar-trellis@users.noreply.github.com>
2020-12-12 20:15:40 -0800

    Created broadcast intent application

--- app/src/main/java/alex/carcar/sendbroadcast/MainActivity.kt
+++ app/src/main/java/alex/carcar/sendbroadcast/MainActivity.kt
@@ -1,11 +1,20 @@
 package alex.carcar.sendbroadcast
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun broadcastIntent(view: View) {
+        val intent = Intent()
+        intent.action = "alex.carcar.sendbroadcast"
+        intent.flags = Intent.FLAG_INCLUDE_STOPPED_PACKAGES
+        sendBroadcast(intent)
     }
 }
